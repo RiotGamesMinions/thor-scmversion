abbf4c14dd0d42f3b6fb9f6ee6a2d00e5ecb5f44
Kyle Allan <kallan@riotgames.com>
2012-10-03 19:11:28 -0700

    replace generic text with a description

--- lib/thor-scmversion.rb
+++ lib/thor-scmversion.rb
@@ -29,7 +29,7 @@ module ThorSCMVersion
     method_option :version_file_path,
       :type => :string,
       :default => nil,
-      :desc => "Foo"
+      :desc => "An additional path to copy a VERSION file to on the file system."
     desc "current", "Show current SCM tagged version"
     def current
       write_version(options[:version_file_path])
